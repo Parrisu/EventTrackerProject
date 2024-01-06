11f45942170cb1522604c17c9cf45cc80a4ca55e
Parrisu <parris.a.jones@gmail.com>
2024-01-06 10:18:31 -0800

    added store search check, added search donuts by storeId

--- Donut/src/main/java/com/skilldistillery/donut/controllers/StoreController.java
+++ Donut/src/main/java/com/skilldistillery/donut/controllers/StoreController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.donut.entities.Donut;
 import com.skilldistillery.donut.entities.Store;
+import com.skilldistillery.donut.repositories.DonutRepository;
 import com.skilldistillery.donut.service.StoreService;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -20,6 +22,9 @@ public class StoreController {
 	@Autowired
 	StoreService storeServ;
 
+	@Autowired
+	DonutRepository donutRepo;
+
 	@GetMapping({ "stores", "stores/" })
 	public List<Store> getStores() {
 		return storeServ.getStoreList();
@@ -27,8 +32,34 @@ public class StoreController {
 
 	@GetMapping("stores/{id}")
 	public Store getStore(@PathVariable("id") int id, HttpServletResponse res) {
-		return storeServ.getStoreById(id);
+		Store store = storeServ.getStoreById(id);
+		if (store != null) {
+			res.setStatus(200);
+		} else {
+			res.setStatus(404);
+		}
+		return store;
 	}
 
+	@GetMapping("stores/{id}/donuts")
+	public List<Donut> getDonutsByStore(@PathVariable("id") int id, HttpServletResponse res) {
+		List<Donut> donuts = null;
+
+		Store store = storeServ.getStoreById(id);
+		if (store != null) {
+			donuts = donutRepo.findByStoreId(id);
+			for (int i = 0; i < donuts.size(); i++) {
+				if (donuts.get(i).isEnabled() == false) {
+					donuts.remove(i);
+				}
+			}
+			res.setStatus(200);
+
+		} else {
+			res.setStatus(404);
+		}
+
+		return donuts;
+	}
 
 }
